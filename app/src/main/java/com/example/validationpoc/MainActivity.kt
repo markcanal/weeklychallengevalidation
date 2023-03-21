42ec55fe72aa74cdb053846c573150c8af2453c6
Mark Jerald Canal <markjeraldc@gmail.com>
2023-03-22 00:17:22 +0800

    :sparkles: UI

--- app/src/main/java/com/example/validationpoc/MainActivity.kt
+++ app/src/main/java/com/example/validationpoc/MainActivity.kt
@@ -1,41 +1,49 @@
 package com.example.validationpoc
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.validationpoc.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private var spinner: Spinner? = null
-    private var userNameContainer: View? = null
-    private var userNameLabel: TextView? = null
-    private var userNameError: TextView? = null
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        initViews()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        initViews(binding)
+        setContentView(view)
     }
 
-    private fun initViews() {
-        userNameContainer = findViewById(R.id.full_name_container)
-        userNameLabel = findViewById<TextView>(R.id.txv_label)
-        userNameError = findViewById(R.id.txv_message_label)
-        userNameLabel.text = applicationContext.getString(R.string.full_name_label)
+    private fun initViews(binding: ActivityMainBinding) {
+        binding.fullNameContainer.txvLabel.text =
+            applicationContext.getString(R.string.full_name_label)
+        binding.emailContainer.txvLabel.text = applicationContext.getString(R.string.email_label)
+        binding.mobileContainer.txvLabel.text =
+            applicationContext.getString(R.string.mobile_number_label)
+        binding.calendarContainer.txvLabel.text =
+            applicationContext.getString(R.string.birthdate_label)
 
-        val emailContainer: View = findViewById(R.id.email_container)
-        val emailLabel: TextView = emailContainer.findViewById(R.id.txv_label)
-        val emailError: TextView = emailContainer.findViewById(R.id.txv_message_label)
-        userNameLabel.text = applicationContext.getString(R.string.email_label)
-//        spinner = findViewById(R.id.gender)
-//        ArrayAdapter.createFromResource(
-//            this, R.array.gender_array, android.R.layout.simple_spinner_dropdown_item
-//        ).also { arrayAdapter ->
-//            arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//            spinner?.adapter = arrayAdapter
-//        }
+        binding.calendarContainer.txeFullName.isFocusable = false
+        binding.calendarContainer.txeFullName.setOnClickListener {
+            Toast.makeText(this, "Click me not", Toast.LENGTH_LONG).show()
+        }
+        binding.ageContainer.txvLabel.text = applicationContext.getString(R.string.age_label)
+        binding.ageContainer.txeFullName.isFocusable = false
+        binding.ageContainer.txeFullName.setText(
+            applicationContext.getString(R.string.age_value_text, ""),
+
+            )
+        binding.genderSelect.adapter =
+            ArrayAdapter.createFromResource(
+                this,
+                R.array.gender_array,
+                android.R.layout.simple_spinner_dropdown_item
+            )
+
     }
 }
